606747d236eecd376d662acd8aa33737132a67ee
gregjones <greg@storyuk.com>
2026-06-16 16:58:16 +0100

    hero responsive

--- parts/block-hero.php
+++ parts/block-hero.php
@@ -13,18 +13,18 @@ $image = get_field('image');
 
 <section id="<?php echo esc_attr($id); ?>">
     <div class="grid">
-        <div class="col-start-1 row-start-1 relative">
+        <div class="col-start-1 lg:row-start-1 relative">
             <?php if ($image) { ?>
                 <div class="
-        before:content-['']
-        before:absolute
-        before:inset-0
-        before:bg-linear-to-r
-        before:from-charcoal-grey
-        before:from-30%
-        before:to-transparent
-        before:to-80% before:z-10">
-                    <div class="absolute top-0 right-0 bottom-0 left-[30%]">
+        lg:before:content-['']
+        lg:before:absolute
+        lg:before:inset-0
+        lg:before:bg-linear-to-r
+        lg:before:from-charcoal-grey
+        lg:before:from-30%
+        lg:before:to-transparent
+        lg:before:to-80% before:z-10">
+                    <div class="lg:absolute lg:top-0 lg:right-0 lg:bottom-0 lg:left-[30%] max-lg:aspect-3/1">
                         <?php
                         $imageID = $image['ID'];
                         echo wp_get_attachment_image($imageID, 'huge', false, array('loading' => 'eager', 'class' => 'w-full h-full object-cover'));
@@ -33,11 +33,11 @@ $image = get_field('image');
                 </div>
             <?php } ?>
         </div>
-        <div class="col-start-1 row-start-1 z-10 text-ivory-white py-25">
+        <div class="col-start-1 lg:row-start-1 z-10 text-ivory-white max-lg:py-4 lg:py-25 max-lg:bg-charcoal-grey">
             <div class="container mx-auto px-4">
                 <div class="grid grid-cols-12 gap-4">
-                    <div class="col-span-7">
-                        <h1 class="font-merriweather text-[4.0625rem] leading-[1.2] font-bold">
+                    <div class="max-lg:col-span-12 lg:col-span-7">
+                        <h1 class="font-merriweather max-lg:text-[calc(35/16*1rem)] lg:text-[calc(65/16*1rem)] leading-[1.2] font-bold">
                             <?php if ($heading) { ?>
                                 <?php echo $heading; ?>
                             <?php } else { ?>
